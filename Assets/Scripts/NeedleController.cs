7316f66be9a92985f64100abe75aafdbacaeea70
tomoya-takubo <tkb1003@gmail.com>
2020-03-22 17:46:17 +0900

    20200322 コミット（3回目）
    コンソールにおみくじ表示
    UIにおみくじ表示実装途中

---
+++ Assets/Scripts/NeedleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleController : MonoBehaviour
+{
+    public string result;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "kyou":
+                result = "凶";
+                Debug.Log("凶");
+                break;
+            case "tyukichi":
+                result = "中吉";
+                Debug.Log("中吉");
+                break;
+            case "suekichi":
+                result = "末吉";
+                Debug.Log("末吉");
+                break;
+            case "syoukichi":
+                result = "小吉";
+                Debug.Log("小吉");
+                break;
+            case "daikyou":
+                result = "大凶";
+                Debug.Log("大凶");
+                break;
+            case "daikichi":
+                result = "大吉";
+                Debug.Log("大吉");
+                break;
+        }
+
+        /***
+        if(collision.tag == "kyou")
+        {
+            Debug.Log("凶です。");
+        }
+        ***/
+    }
+}
